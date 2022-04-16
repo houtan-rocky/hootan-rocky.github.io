2d4fe88523da4c5dfda591c202b7b002b2b34db3
Hootan Abasi Rocky <mast9rmind@gmail.com>
2022-04-16 19:26:19 +0430

    change div to semantic and add language logos and changed nav bar colors

--- js/index.js
+++ js/index.js
@@ -3,3 +3,14 @@ object = {
 	lastName: "Abasi",
 	age: 25,
 };
+
+const getDateInFormat = (inputDate) => {
+	const YYYY = inputDate.getFullYear();
+	const MM = inputDate.getMonth();
+	const DD = inputDate.getDate();
+	return YYYY + "/" + MM + "/" + DD;
+};
+
+document.getElementById(
+	"time"
+).innerHTML = `last time updated : ${getDateInFormat(new Date(2022, 4, 16))} `;
