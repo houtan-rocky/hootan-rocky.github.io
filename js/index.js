918429b5ac8cd1e9357c1dd3174da68744d524e5
Hootan Abasi Rocky <mast9rmind@gmail.com>
2022-04-16 20:08:45 +0430

    added login user password

--- js/index.js
+++ js/index.js
@@ -14,3 +14,11 @@ const getDateInFormat = (inputDate) => {
 document.getElementById(
 	"time"
 ).innerHTML = `last time updated : ${getDateInFormat(new Date(2022, 4, 16))} `;
+
+userLogin = { username: "hootan", password: "1234" };
+let username = prompt("please enter your username");
+let password = prompt(`please enter password for username ${username}`);
+if (!(username === userLogin.username && password === userLogin.password)) {
+	document.getElementById("body").innerHTML =
+		"You aren't allowed to visit this page";
+}
